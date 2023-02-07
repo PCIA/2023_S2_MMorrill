4a84462c34cb63f61eeaa370888ad848bd207c26
Matthew Morrill <100809031+ChimpWithAMachineGun@users.noreply.github.com>
2023-02-07 12:05:25 -0800

    Learned how to randomize Boolean values

--- LittleTimmy.py
+++ LittleTimmy.py
@@ -1,1 +1,9 @@
+import random
 print("Hello world, this is Matthew's firstcode!")
+name = "little_timmy"
+print(name)
+isRaining = random.choice([True, False])
+if isRaining == True:
+    print("It's raining cats and dogs!")
+if isRaining == False:
+    print("We should take a walk.")
