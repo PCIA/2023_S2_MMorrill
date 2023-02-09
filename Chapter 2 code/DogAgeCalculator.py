6ef6e1a78aab1762e85877161599bdaf13fda993
Matthew Morrill <100809031+ChimpWithAMachineGun@users.noreply.github.com>
2023-02-09 09:08:19 -0800

    Correctly calculates dog age as integer

---
+++ Chapter 2 code/DogAgeCalculator.py
@@ -0,0 +1,5 @@
+print("Hello! Welcome to the Dog Age Calculator!")
+dog_name = input("What is your dog's name?")
+dog_age = input("What is your dog's age?")
+human_age = int(dog_age)*7
+print("Your dog", dog_name, "is", human_age, "human years old!")
