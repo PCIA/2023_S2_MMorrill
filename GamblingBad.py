df8711a266046d4e71b27e8ad397b82ab0679214
Matthew Morrill <100809031+ChimpWithAMachineGun@users.noreply.github.com>
2023-02-10 11:21:04 -0800

    Added money and slots

--- GamblingBad.py
+++ GamblingBad.py
@@ -1,5 +1,6 @@
 import time
 import random
+PlayerMoney = 0
 print("'Hey Kids! Today we'll be learning about why gambling is bad.'")
 time.sleep(1.25)
 answer = input("Are you ready? [Yes or No] ")
@@ -13,9 +14,14 @@ time.sleep(1.25)
 choice1 = input("Do you spin the machine? [Yes or No] ")
 if choice1 == "Yes": print('*The machine rumbles and spins*')
 else: print('*The instructor pulls the lever for you*')
-SlotValue = ("Cherry","7","King","Queen","Diamond")
-Slot1 = random.choice(SlotValue)
-Slot2 = random.choice(SlotValue)
-Slot3 = random.choice(SlotValue)
+slotvalue = ("Cherry","7","King","Queen","Diamond")
+slot1 = random.choice(slotvalue)
+slot2 = random.choice(slotvalue)
+slot3 = random.choice(slotvalue)
 time.sleep(2)
-print("You got", Slot1, Slot2, "and", Slot3)
+print("You got", slot1, slot2, "and", slot3)
+if slot1 == slot2 or slot3: 
+    print("Congratulations you've won 10 coins!")
+    PlayerMoney = PlayerMoney + 10
+time.sleep(1.25)
+print("You now have", PlayerMoney,"Gold Coins")
