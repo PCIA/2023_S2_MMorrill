aa491e752eb6e6a7f37c6fca0b19e0568e8a8e16
Matthew Morrill <100809031+ChimpWithAMachineGun@users.noreply.github.com>
2023-02-14 12:11:17 -0800

    Fixed Reward system

--- GamblingBad.py
+++ GamblingBad.py
@@ -4,7 +4,7 @@ userplay = "yes"
 playermoney = 0
 print("'Hey Kids! Today we'll be learning about why gambling is bad.'")
 time.sleep(1.25)
-answer = input("Are you ready? [Yes or No] ")
+answer = input("Are you ready? [Yes or No] ").upper()
 if answer == "Yes": print("'Good. Let us begin.'")
 if answer == "No": print("'Too bad! We're starting.'")
 time.sleep(1.25)
@@ -13,8 +13,8 @@ while userplay == "yes":
     time.sleep(1.25)
     print("'Alright, now spin the machine.'")
     time.sleep(1.25)
-    choice1 = input("Do you spin the machine? [Yes or No] ")
-    if choice1 == "Yes": print('*The machine rumbles and spins*')
+    choice1 = input("Do you spin the machine? [Yes or No] ").upper()
+    if choice1 == "YES": print('*The machine rumbles and spins*')
     else: print('*The instructor pulls the lever for you*')
     slotvalue = ("Cherry","7","King","Queen","Diamond")
     slot1 = random.choice(slotvalue)
@@ -22,12 +22,17 @@ while userplay == "yes":
     slot3 = random.choice(slotvalue)
     time.sleep(2)
     print("You got", slot1, slot2, "and", slot3)
-    if slot1 == slot2 or slot3: 
-        print("Congratulations you've won 10 coins!")
-        playermoney = playermoney + 10
+    if not slot1 == slot2 and not slot1 == slot2: basicreward = 0
+    if slot1 == slot2 and not slot1 == slot3: basicreward = 1
+    if slot1 == slot3 and not slot1 == slot2: basicreward = 1
     if slot1 == slot2 and slot2 == slot3: 
         print("Congrats! You got a jackpot!")
         playermoney = playermoney + 100
+    if basicreward == 1: 
+        print("Congratulations you've won 10 coins!")
+        playermoney = playermoney + 10
+    if basicreward == 0:
+        print("Sorry you didn't win anything! ")
     time.sleep(1.25)
     print("You now have", playermoney,"Gold Coins")
     playagain = input ("Would you like to play again? [Yes or No]").upper()
