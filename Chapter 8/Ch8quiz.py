6bedb70ce938bc6fb3a701cf0321842af3e59139
Matthew Morrill <100809031+ChimpWithAMachineGun@users.noreply.github.com>
2023-04-20 09:35:54 -0700

    Quiz success

---
+++ Chapter 8/Ch8quiz.py
@@ -0,0 +1,13 @@
+words = {}
+words['large'] = {'definition':'Having much size.', 'synonyms':['big','volumous','grande'],'UseCount':5}
+words['small'] = {'definition':'Having low size.', 'synonyms':['little','mini'],'UseCount':10}
+words['gray'] = {'definition':'A neutral color that is neither black nor white.', 'synonyms':['grey','ash'],'UseCount':15}
+
+def averageWordUse():
+   total = 0
+   for word in words:
+      currentWord = words[word]
+      total = total + int(currentWord['UseCount'])
+   return total / len(words)
+
+print(averageWordUse())
---
+++ Chapter 8/RecursiveFunctions.py
@@ -0,0 +1,11 @@
+def is_palindrome(word):
+    if len(word) <= 1:
+        return True
+    else:
+        if word[0] == word[-1]:
+            return is_palindrome(word[1:-1])
+        else:
+            return False
+words = ['tacocat','radar','preworkout','wok','pepep']
+for word in words:
+    print(word,is_palindrome(word))
